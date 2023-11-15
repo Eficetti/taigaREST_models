02d7c02ef4b45f5e689406008cebe87ca20d6713
NonKill <69329781+N-KILL@users.noreply.github.com>
2023-11-15 09:49:31 -0300

    Tags mapper

--- lib/src/models/change/tags.dart
+++ lib/src/models/change/tags.dart
@@ -1,11 +1,19 @@
 /// This class is used to storage the changes in the Tags
 class Tags {
 
-    /// Constructor
+    /// Constructor of the Tags class
     Tags({
         required this.to,
         required this.from,
     });
+
+    /// Mapper of the Tags class
+    factory Tags.fromJson(Map<String, dynamic> json) {
+    return Tags(
+      from: List<String>.from(json['from'] as List<dynamic>),
+      to: List<String>.from(json['to'] as List<dynamic>),
+    );
+  }
 
     /// Old Value
     List<String> from;
