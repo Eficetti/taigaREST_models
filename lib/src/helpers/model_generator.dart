a9d0c00b243deb5a5dd2b505668b0edc3b6aee31
NonKill <club_dog2@hotmail.com>
2023-11-16 11:43:30 -0300

    taigaObjectGenerator function added

---
+++ lib/src/helpers/model_generator.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
+import 'package:taiga_rest_models/src/models/taiga_issue/taiga_issue.dart';
+import 'package:taiga_rest_models/src/models/taiga_user_story/taiga_user_story.dart';
+
+/// This function receives the information from the taiga webhook as a JSON in
+/// String format, transforms it into a Map<String, dynamic> and then reads its
+/// values them it converts them into an object (can be an "Issue"/"User_Story")
+/// which will be returned upon completion
+dynamic taigaObjectGenerator(String jsonString) {
+  final jsonData = json.decode(jsonString) as Map<String, dynamic>;
+  final payloadType = jsonData['type'];
+  // final payloadAction = jsonData['action'];
+  if (payloadType == 'issue') {
+    final payloadIssue = TaigaIssue.fromJson(jsonData);
+    print('Detected issue type $payloadIssue');
+    return payloadIssue;
+  }
+  if (payloadType == 'userstory') {
+    final payloadUserStory = TaigaUserStory.fromJson(jsonData);
+    print('Detected userStory type $payloadUserStory');
+    return payloadUserStory;
+  }
+}
--- lib/taiga_rest_models.dart
+++ lib/taiga_rest_models.dart
@@ -1,6 +1,7 @@
 /// TaigaRest models
 library taiga_rest_models;
 
+export 'src/helpers/model_generator.dart';
 export 'src/helpers/validate_taiga_signature.dart';
 export 'src/models/taiga_issue/taiga_issue.dart';
 export 'src/models/taiga_user_story/taiga_user_story.dart';
