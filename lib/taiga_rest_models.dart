bb22e4718d17cf7481f82b288f65698247312add
NonKill <club_dog2@hotmail.com>
2023-11-30 11:01:04 -0300

    export data wiki

--- lib/taiga_rest_models.dart
+++ lib/taiga_rest_models.dart
@@ -3,6 +3,7 @@ library taiga_rest_models;
 
 export 'src/helpers/validate_taiga_signature.dart';
 export 'src/models/taiga_payload/taiga_data/data_sprint/data_sprint.dart';
+export 'src/models/taiga_payload/taiga_data/data_wiki/data_wiki.dart';
 export 'src/models/taiga_payload/taiga_data/taiga_data.dart';
 export 'src/models/taiga_payload/taiga_payload.dart';
 export 'src/taiga_rest_models.dart';
